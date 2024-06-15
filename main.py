7c201d7fc66ab0bbe9b2fddb93834417d0213508
RuslanHale <ruskhal333@gmail.com>
2024-06-16 01:55:41 +0500

    working version

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import words_file
+
+
 def alphabet_selection(input_word, selected_alphabet):
+
     input_word = input_word.lower()
     for i in range(0, len(input_word)-1, 2):
         if input_word[i] == '=':
@@ -12,37 +15,37 @@ def alphabet_selection(input_word, selected_alphabet):
     return selected_alphabet
 
 
-def word_selection(selected_alphabet, selected_words,  letter_positions):
-
+def word_selection(selected_alphabet, selected_words):
     for key, val in selected_alphabet.items():
         for sym in val.keys():
-            print(selected_alphabet[key][sym])
             if sym == '=':
                 for index in selected_alphabet[key][sym]:
                     selected_words = list(filter(lambda x: x[index] == key, selected_words))
             elif sym == '+':
                 for index in selected_alphabet[key][sym]:
-                    selected_words = list(filter(lambda x: x[index] != key and len(selected_alphabet[key][sym]) == x.count(key), selected_words))
+                    selected_words = list(filter(lambda x: x[index] != key and len(selected_alphabet[key][sym] + selected_alphabet[key].get('=', list())) == x.count(key), selected_words))
             elif sym == '-':
                 for index in selected_alphabet[key][sym]:
-                    selected_words = list(filter(lambda x: x[index] != key, selected_words))
+                    selected_words = list(filter(lambda x: x[index] != key and not (key in x and len(selected_alphabet[key]) == 1), selected_words))
+
     return selected_words
 
 
-word = '-Д=А=М-Б+А'
+word = input('Введите новое слово: ')
 alphabet = {chr(i): dict() for i in range(ord('а'), ord('а')+32)}
 words = words_file.data
-positions = list(range(5))
 
 
 while word != 'stop':
     print(word)
-    print(len(words))
     alphabet = alphabet_selection(word, alphabet)
-    words = word_selection(alphabet, words, positions)
+    words = word_selection(alphabet, words)
+    alphabet = {chr(i): dict() for i in range(ord('а'), ord('а')+32)}
     print(words)
-    print(len(words))
     word = input('Введите новое слово: ')
 
-# Статус буквы заменятся в функции alphabet_selection. Имеет смысл сделать значением список и идти по списку
-# например буква А: с '=' изменяется на '+'
+# +П-А-Л-А-Ч
+# -В-Е+П+Р-Ь
+# -И=П=Р-И-Т
+# -О=П=Р=О=С
+# Нет смысла идти по всем 33 буквам алфавита. Можно переделать вообще не используя алфавит.
